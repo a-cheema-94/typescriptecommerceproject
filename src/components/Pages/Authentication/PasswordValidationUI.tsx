8097266c33dc60c0d82999943f1f44aff121f322
Amar Cheema <amr.cheema@yahoo.com>
2025-01-17 22:11:20 +0000

    Finished password validation on SignUp component. Will refactor.

--- src/components/Pages/Authentication/PasswordValidationUI.tsx
+++ src/components/Pages/Authentication/PasswordValidationUI.tsx
@@ -1,46 +1,77 @@
 import verifyPassword from "../../../utilities/verifyPassword";
+import { PasswordValidationStatus } from "firebase/auth";
 
 type Props = {
   password: string;
 };
 
-// todo => sync passwordStatus data to display in UI.
+// todo => refactor password validation functions and component.
 
 const PasswordValidationUI = ({ password }: Props) => {
+  const passwordStatus: PasswordValidationStatus | null =
+    verifyPassword(password);
 
-  
-  const passwordStatus = verifyPassword(password);
+  const getPasswordValidationCategories = (
+    statusObject: PasswordValidationStatus | null
+  ) => {
+    const validationKeys = Object.keys(statusObject ?? {});
+    const validationValues = Object.values(statusObject ?? {});
+    const mappedKeys = validationKeys.map((passwordCategory, index) => {
+      let editedCategory = passwordCategory
+        .replace(/([a-z])([A-Z])/g, "$1" + " " + "$2")
+        // replace all instances of a lower case character directly followed by an upper case character with the lower case followed by a space and then the upper case. $i references the ith character.
+        .toLowerCase();
+      const validationValue = validationValues[index];
+      return { editedCategory, validationValue };
+    });
 
-  const samplePasswordValidationObject = {
-    isValid: true,
-    meetsMinPasswordLength: true,
-    meetsMaxPasswordLength: true,
-    containsLowercaseLetter: true,
-    containsUppercaseLetter: true,
-    containsNumericCharacter: true,
-    containsNonAlphanumericCharacter: true,
-    test: { test: 'test' }
+    return mappedKeys.filter(
+      (data) => data.editedCategory !== "password policy"
+    );
   };
+
+  let passwordCategories: {
+    editedCategory: string;
+    validationValue: boolean;
+  }[] = [];
+  if (passwordStatus)
+    passwordCategories = getPasswordValidationCategories(passwordStatus);
 
   return (
     <div>
       <h3>Password Validation</h3>
-      <p
-        className={
-          passwordStatus?.containsUppercaseLetter === true
-            ? "text-green-600"
-            : "text-red-600"
-        }
-      >
-        contains uppercase letters
-        {passwordStatus?.containsUppercaseLetter === true ? (
-          <span>✔</span>
-        ) : (
-          <span>❌</span>
-        )}
-      </p>
+      {passwordCategories.map((category, index) => (
+        <PasswordValidationCategory category={category} key={index}/>
+      ))}
     </div>
   );
+};
+
+const PasswordValidationCategory = ({
+  category,
+}: {
+  category: {
+    editedCategory: string;
+    validationValue: boolean;
+  };
+}) => {
+  return (
+    <p
+    className={
+      category.validationValue === true
+        ? "text-green-600"
+        : "text-red-600"
+    }
+  >
+    {category.editedCategory}
+    {category.validationValue === true ? (
+      <span>✔</span>
+    ) : (
+      <span>❌</span>
+    )}
+  </p>
+  )
+  
 };
 
 export default PasswordValidationUI;
